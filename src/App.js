919f72cd033efd633eaf8ffae7a028b89f58b7d5
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-12-25 16:36:56 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import { HeaderNavbar, ClippedDrawer } from '@doc-api/components'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { routes } from '@doc-api/routes'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <HeaderNavbar></HeaderNavbar>
+        <ClippedDrawer></ClippedDrawer>
+        <main className={classes.content}>
+          <Toolbar />
+          <Switch>
+            {routes.map((route, index) => (
+              // Render more <Route>s with the same paths as
+              // above, but different components this time.
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                children={<route.main />}
+              />
+            ))}
+          </Switch>
+        </main>
+      </div>
+    </Router>
+  );
+}
---
+++ src/components/ClippedDrawer/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import { CustomizedTreeView } from '@doc-api/components'
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerContainer: {
+        overflow: 'auto',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
+
+export default function ClippedDrawer() {
+    const classes = useStyles();
+
+    return (
+        <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+                <CustomizedTreeView></CustomizedTreeView>
+            </div>
+        </Drawer>
+
+    );
+}
